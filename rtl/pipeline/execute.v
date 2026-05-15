6259e366bb9eb60ef56216292fae7ee010a382ec
JosephAS06 <joseph.alvarez06as@gmail.com>
2026-05-15 08:41:09 +0000

    Se agrega la segunda etapa del pipeline en la cual se realizarán las operaciones, esta primera versión unicamente tiene soporte para 5 instrucciones

---
+++ rtl/pipeline/execute.v
@@ -0,0 +1,38 @@
+module s2_execute (
+    input         clk,
+    input         rst,
+
+    // From Stage 1 pipeline registers
+    input         i_valid,
+    input  [2:0]  i_alu_op,
+    input  [4:0]  i_rd,
+    input  [127:0] i_vs1_data,
+    input  [127:0] i_vs2_data,
+
+    // Pipeline registers → Stage 3
+    output reg        o_valid,
+    output reg [4:0]  o_rd,
+    output reg [127:0] o_result
+);
+
+wire [127:0] alu_out;
+
+    alu_array #(.SIZE(32), .N(4)) alu (
+        .alu_op (i_alu_op),
+        .in_a   (i_vs1_data),
+        .in_b   (i_vs2_data),
+        .out    (alu_out)
+    );
+
+    always @(posedge clk) begin
+        if (rst) begin
+            o_valid  <= 1'b0;
+            o_rd     <= 5'b0;
+            o_result <= 128'b0;
+        end else begin
+            o_valid  <= i_valid;
+            o_rd     <= i_rd;
+            o_result <= alu_out;
+        end
+    end
+endmodule
